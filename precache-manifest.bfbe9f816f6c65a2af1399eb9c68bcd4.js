409145613c7c93066b8e477ec7b2f8c36c34b640
James_Ferreira <jameswferreira@gmail.com>
2020-07-13 13:57:37 +1000

    Updates

---
+++ precache-manifest.bfbe9f816f6c65a2af1399eb9c68bcd4.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "413fe2c1885eee39ab9e20ab4465720d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ded40906c45c0828af52",
+    "url": "/portfolio/static/css/main.e2ad93e9.chunk.css"
+  },
+  {
+    "revision": "8f339f21e425474f98b6",
+    "url": "/portfolio/static/js/2.571dc36d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.571dc36d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ded40906c45c0828af52",
+    "url": "/portfolio/static/js/main.9ff75e77.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "86fa44a22a6394132304544d6e2efc22",
+    "url": "/portfolio/static/media/Bumble-Logo.86fa44a2.png"
+  },
+  {
+    "revision": "c8f35daa05d411916abd4de0e16a62dc",
+    "url": "/portfolio/static/media/InputMono-Regular.c8f35daa.ttf"
+  },
+  {
+    "revision": "1ebe43d0347596444a1c8ff897b9f0d5",
+    "url": "/portfolio/static/media/KarmaFuture.1ebe43d0.ttf"
+  },
+  {
+    "revision": "a19addc97dd8189ed8eb0496fe65709b",
+    "url": "/portfolio/static/media/LinkedIn-Logo.a19addc9.png"
+  },
+  {
+    "revision": "5fcd6c657ffa8a6791632c705f0944ea",
+    "url": "/portfolio/static/media/VcrMono.5fcd6c65.ttf"
+  },
+  {
+    "revision": "d4eada3e07e6681126b75343c4657ec5",
+    "url": "/portfolio/static/media/dither-border.d4eada3e.png"
+  },
+  {
+    "revision": "eb164c9402822533ad94861587c7ce7c",
+    "url": "/portfolio/static/media/dither_tile.eb164c94.png"
+  },
+  {
+    "revision": "d77c2e1165534a3a0ceddac5343bf3b0",
+    "url": "/portfolio/static/media/home-dither-border.d77c2e11.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a5c3a813f9db4baec34d5fcbab6e4b3e",
+    "url": "/portfolio/static/media/pixel-about.a5c3a813.png"
+  },
+  {
+    "revision": "479d46c85cc81bb9c01338e88f53fc81",
+    "url": "/portfolio/static/media/pixel-award.479d46c8.png"
+  },
+  {
+    "revision": "64dded8b9e93533e9401d4ea480804f7",
+    "url": "/portfolio/static/media/pixel-badge.64dded8b.png"
+  },
+  {
+    "revision": "cac5b83c97846b5e07446afbb374a2e5",
+    "url": "/portfolio/static/media/pixel-book.cac5b83c.png"
+  },
+  {
+    "revision": "b50374cbf8f39187409030098e623aa4",
+    "url": "/portfolio/static/media/pixel-branch.b50374cb.png"
+  },
+  {
+    "revision": "c305b0b1fa1fe33cb9b6384c4251e7e2",
+    "url": "/portfolio/static/media/pixel-glasses.c305b0b1.png"
+  },
+  {
+    "revision": "1c3abdee848eaea97bf4c044a59c74d2",
+    "url": "/portfolio/static/media/pixel-mug.1c3abdee.png"
+  },
+  {
+    "revision": "eee98215eb64423d15032cab8f4465fb",
+    "url": "/portfolio/static/media/pixel-name.eee98215.png"
+  },
+  {
+    "revision": "3fe28f0a01f87b9e7b1da7c6d84ead04",
+    "url": "/portfolio/static/media/pixel-profile.3fe28f0a.png"
+  },
+  {
+    "revision": "f987d81425565c673df7fd2c2cfa980f",
+    "url": "/portfolio/static/media/pixel-projects.f987d814.png"
+  },
+  {
+    "revision": "f7ef50b76fe464328ed8fdcb86d62586",
+    "url": "/portfolio/static/media/pixel-record.f7ef50b7.png"
+  },
+  {
+    "revision": "252cbe7a522d476d63f3d55f041779cb",
+    "url": "/portfolio/static/media/pixel-solder.252cbe7a.png"
+  },
+  {
+    "revision": "e290c346d5eecb2c91058d738b5f9342",
+    "url": "/portfolio/static/media/pixel-textbook.e290c346.png"
+  },
+  {
+    "revision": "c2fb38f52d8a56766f21276a130a9fce",
+    "url": "/portfolio/static/media/pixel-wasd.c2fb38f5.png"
+  },
+  {
+    "revision": "d54d1d1fc68871bcfbe68556bbf663e9",
+    "url": "/portfolio/static/media/profile.d54d1d1f.jpg"
+  }
+]);
